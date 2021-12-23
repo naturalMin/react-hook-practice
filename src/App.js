16ff070e2ca30991d78486c75c8428d8a46c3f6c
jungmin kim <88076945+naturalMin@users.noreply.github.com>
2021-12-23 15:45:00 +0900

    useRef & unmount practice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import "./styles.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]); //title이 업데이트되면 updateTitle호출
-  return setTitle;
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onClick !== "function") {
+      return;
+    }
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    } //mount
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    }; //unmount
+  }, []); //단 한번만 실행
+  return typeof onClick !== "function" ? element : undefined;
 };
+// React 16.8v 부터는 Hook 을 조건문, 반복문, 중첩함수 내에서 호출할 수 없습니다.
 
 export default function App() {
-  const titleUpdater = useTitle("Loading..."); //브라우저 타이틀 Loading..으로 변경됨.
-  setTimeout(() => titleUpdater("Home"), 5000); //5초후 브라우저 타이틀 Home으로 변경
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+  //const input = useRef(); //document.getElementById()와 동일
+  //setTimeout(() => input.current?.focus(), 5000); //초반에 undefined로 잡혀서 ?.처리
   return (
     <div className="App">
-      <div>Hi</div>
+      <h1 ref={title}>Hi</h1>
+      {/* <input ref={input} placeholder="hi" /> */}
     </div>
   );
 }
