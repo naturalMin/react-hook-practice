ade5f58c57605c0f3b1f8b8e332a5acf292acffc
jungmin kim <88076945+naturalMin@users.noreply.github.com>
2021-12-23 15:07:10 +0900

    useEffect example Recap

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
 import "./styles.css";
-import { useState } from "react";
-
-//content api 예시
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1"
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2"
-  }
-];
-
-const useTabs = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab); //최상위에서 hook호출 => 안그럼 에러뜸.
-  if (!allTabs || !Array.isArray(allTabs)) {
-    //필수적인 매개변수가 빠질때 에러 검출 (2번째조건: allTabs의 배열이 false이거나)
-    //여기서 Array잎에 !를 빼먹는 실수를 해서 typeError가 떴다. 꼼꼼히 코딩하자!
-    return; //문제없으면 조용.
-  }
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex //버튼을 클릭하면 index를 바꿔줌.
-  };
-};
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const { currentItem, changeItem } = useTabs(0, content);
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const [aNumber, setANumber] = useState(0);
+  useEffect(() => {
+    sayHello(); //componentDidMount // 최초에 한 번만 작동. mount = 행위자체 보여진다.
+  }, [number]); //componentDidUpdate //number가 mount될때만 호출. 한번만 호출=> []
   return (
     <div className="App">
-      {content.map((section, index) => (
-        <button key={index} onClick={() => changeItem(index)}>
-          {/* key값은 반드시 넣어줘야한다. 안넣으면 경고함 */}
-          {section.tab}
-        </button>
-      ))}
-      {/* onClick = {changeItem}으로 사용하면 자동으로 함수 호출 -> Too many re-renders라는 무한루프 에러가 뜬다. */}
-      <div>{currentItem.content}</div>
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>add</button>
+      <button onClick={() => setANumber(aNumber + 1)}>add2</button>
+      <div>{number}</div>
+      {/* number만 클릭할 수록 랜더링 진행 */}
+      <div>{aNumber}</div>
+      {/* aNumber 랜더링안됨 */}
     </div>
   );
 }
