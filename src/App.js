1130d0089c392e2bd7e635417af92a8d9dcdd21c
jungmin kim <88076945+naturalMin@users.noreply.github.com>
2021-12-25 01:05:20 +0900

    usePreventLeave practice

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import "./styles.css";
 import { useState, useEffect, useRef } from "react";
 
-const useConfirm = (message = "", callback, rejection) => {
-  if (typeof callback !== "function") {
-    return; //callback이 함수가 아니라면 그냥 리턴.
-  }
-  const confirmAction = () => {
-    if (window.confirm(message)) {
-      //confirm window창이 browser에 message를 가지고 있다면\
-      //window 추가안하면 Unexpected use of 'confirm' no-restricted-globals 에러
-      callback(); //callback 함수를 실행하라
-    } else {
-      try {
-        rejection(); //그렇지 않다면 rejection을 실행하라.
-        //cancel 누르는 순간 TypeError rejection is not a function 에러발생
-      } catch {
-        return; //typeError 해결: 예외문 작성.
-        //rejection이 필수가 아니라 rejection이 없는 경우에도 Cancel 누르면 실행 될건데 예외 발생으로 프로그램 터질 겁니다.
-      }
-    }
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = ""; //요즘은 Deprecated
   };
-  return confirmAction; //confirmAction 함수값을 리턴하라.
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  // 정말로 이 사이트를 나갈꺼냐? 재확인 질문해줌. 보호역할
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
 };
 
 export default function App() {
-  const deleteWorld = () => console.log("Deleting the world.");
-  const abort = console.log("Aborted");
-  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
+  const { enablePrevent, disablePrevent } = usePreventLeave();
   return (
     <div className="App">
-      <button onClick={confirmDelete}>Delete the world</button>
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
     </div>
   );
 }
